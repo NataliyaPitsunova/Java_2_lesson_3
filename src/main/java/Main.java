d6453ffa5068d14f22f330a91075d56acb839bbe
NataliyaPitsunova <nataillar1988@icloud.com>
2022-02-03 13:37:57 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,36 @@
+import phone.Phone;
+
+import java.lang.reflect.Array;
+import java.text.ParseException;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws ParseException {
+
+        System.out.println("1 задание  - Словарь");
+        System.out.println();
+        ArrayList<String> fruits = new ArrayList<>();
+        fruits.addAll(List.of("Арбуз", "Банан", "Апельсин", "Лимон", "Гуава", "Апельсин", "Ананас", "Банан", "Кокос", "Крыжовник", "Дыня", "Яблоко"));
+        LinkedHashSet<String> lhs = new LinkedHashSet<>(fruits);
+        for (String value : lhs
+        ) {
+            int count = Collections.frequency(fruits, value);
+            System.out.println(value + " " + count);
+        }
+        System.out.println();
+        System.out.println("2 задание - Телефонный справочник");
+        System.out.println();
+
+        Phone phoneBook = new Phone();
+        phoneBook.add("Иванов", "1234548791");
+        phoneBook.add("Иванов", "1236548782");
+        phoneBook.add("Сидоров", "1237548793");
+        phoneBook.add("Абрамов", "4568711985");
+        phoneBook.add("Иванов", "9876548798");
+        phoneBook.add("Ковалев", "6857548773");
+
+        phoneBook.get("Иванов");
+        phoneBook.get("Ковалев");
+    }
+}
+
---
+++ src/main/java/Phone/Phone.java
@@ -0,0 +1,50 @@
+package phone;
+
+import javax.swing.text.MaskFormatter;
+import java.text.ParseException;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class Phone {
+
+
+    private LinkedHashMap<String, String> phoneBook = new LinkedHashMap<>();
+
+    public Phone() {
+        this.phoneBook = phoneBook;
+    }
+
+    public LinkedHashMap<String, String> getPhoneBook() {
+        return phoneBook;
+    }
+
+    public void add(String lastName, String phoneNumber) throws ParseException {
+        if (phoneBook == null) {
+            phoneBook = new LinkedHashMap<String, String>();
+        }
+
+
+        String phoneMask = "###-###-####";
+        MaskFormatter maskFormatter = new MaskFormatter(phoneMask);
+        maskFormatter.setValueContainsLiteralCharacters(false);
+        String formatNumber = maskFormatter.valueToString(phoneNumber);
+
+        phoneBook.put(formatNumber, lastName);
+    }
+
+    public void get(String lastName) {
+        Boolean find = false;
+        for (Map.Entry<String, String> user : phoneBook.entrySet()
+        ) {
+            if (user.getValue() == lastName) {
+                System.out.println(user.getKey() + " " + user.getValue());
+                find = true;
+            }
+        }
+        System.out.println();
+        if (!find) {
+            System.out.println("Абонента с такой фамилиейнет в справочнике");
+        }
+    }
+}
+
